05f6c83c0081c4b3089d93b57760e40d8ebf1a4d
Clonetrash <jamison.bassett@gmx.de>
2020-11-23 13:12:33 +0100

    uhhh Movemenet and basic combat works

---
+++ Anthravaan/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject enemy;
+    public int maxHealth = 100;
+    private int currenthealth; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currenthealth -= damage;
+        //PlayerController hurt anim
+
+        if(currenthealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+        //Die animation
+
+        //disable
+        enemy.SetActive(false);
+        this.enabled = false;
+    }
+}
---
+++ Anthravaan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public int attack = 75;
+    public Rigidbody2D body;
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Combat();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        body.velocity = new Vector2(moveBy, body.velocity.y);
+    }
+
+    void Combat()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        //play anamiation
+        animator.SetTrigger("Attack");
+       
+        //detec enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+        
+        //damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attack);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
